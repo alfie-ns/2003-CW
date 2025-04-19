81ca4c8ccd3a908314e5f33e991a7ef187dbc23c
Alfie Nurse <alfienurse@gmail.com>
2025-04-19 18:46:30 +0100

    Minor: Redeploy API
    Only define Render URL as allowed host for security

--- API/casino_simulator_api/main/settings.py
+++ API/casino_simulator_api/main/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from decouple import config
 
-print(config("OPENAI_API_KEY"))  # Print the OPENAI_API_KEY
-
 OPENAI_API_KEY = config("OPENAI_API_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -30,8 +28,7 @@ SECRET_KEY = 'django-insecure-4a_$qpant-f0!qs@6u6f$u2fg%vt-=q0u88m$(+6$w7rm5gczd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*'] # allow all hosts so can deploy on Render
-
+ALLOWED_HOSTS = ['two003-cw.onrender.com'] # add render url only for security
 
 # Application definition
 
